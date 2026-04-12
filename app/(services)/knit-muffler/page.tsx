f615c244713f727c67e8b9d7c70a1b2091d6c0a4
amaran-th <songsy405@naver.com>
2026-04-12 17:30:51 +0900

    feat: 파비콘 수정

--- app/(services)/knit-muffler/page.tsx
+++ app/(services)/knit-muffler/page.tsx
@@ -4,6 +4,30 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "뜨개뜨개",
   description: "도안을 따라 뜨거나 자유롭게 색을 골라 작품을 완성하는 인터랙션",
+
+  icons: {
+    icon: "/playground/knit-muffler.svg",
+  },
+
+  openGraph: {
+    title: "뜨개뜨개",
+    description:
+      "도안을 따라 뜨거나 자유롭게 색을 골라 작품을 완성하는 인터랙션",
+    images: [
+      {
+        url: "/playground/knit-muffler.png",
+        alt: "뜨개뜨개 미리보기 이미지",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "뜨개뜨개",
+    description:
+      "도안을 따라 뜨거나 자유롭게 색을 골라 작품을 완성하는 인터랙션",
+    images: ["/playground/knit-muffler.png"],
+  },
 };
 
 export default function KnitMufflerPage() {
